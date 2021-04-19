dbaebd57a00faa2d36d2380229a48308be50a8f6
HiFeV <67747114+HiFeV@users.noreply.github.com>
2021-04-19 16:33:02 +0200

    Add files via upload

---
+++ scan.py
@@ -0,0 +1,18 @@
+import socket
+
+host = input('Target to scan: ')
+ip_res = socket.gethostbyname(host)
+
+
+scanned_ports = [20, 21, 22, 23, 25, 53, 80, 110, 119, 123, 143, 161, 194, 443]
+for port in scanned_ports:
+    s = s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.1)
+    try:
+        connection = s.connect_ex((host, port))
+        if connection == 0:
+            print('Port {} OPEN'.format(port))
+        connection.close()
+        s.close()
+    except:
+        pass
