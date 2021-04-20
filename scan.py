9c420996a9d1823981de84ce14f84e17c56b64e3
HiFeV <67747114+HiFeV@users.noreply.github.com>
2021-04-20 18:03:21 +0200

    Update scan.py

--- scan.py
+++ scan.py
@@ -6,7 +6,7 @@ ip_res = socket.gethostbyname(host)
 
 scanned_ports = [20, 21, 22, 23, 25, 53, 80, 110, 119, 123, 143, 161, 194, 443]
 for port in scanned_ports:
-    s = s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(0.1)
     try:
         connection = s.connect_ex((host, port))
